6561651c68179ad6b568cb056d2803725e79c72c
teeswar92 <63140089+teeswar92@users.noreply.github.com>
2020-04-10 17:28:39 +0530

    Update outlier.py

--- outlier.py
+++ outlier.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import LocalOutlierFactor
 
 data = pd.read_csv("creditcard.csv")
-# sampling random 50000 points
-data_50000 = data.sample(n = 50000)
+# sampling random 500 points
+data_50000 = data.sample(n = 500)
 data_50000.to_csv("NewCreditCard.csv")
 newData = pd.read_csv("NewCreditCard.csv")
 FinalData = newData.drop("Unnamed: 0", axis = 1)
@@ -17,7 +17,7 @@ print(outlierArray)
 
 countOutliers = 0
 countInliers = 0
-for i in range(30000):
+for i in range(300):
     if outlierArray[i] == -1:
         countOutliers += 1
     else:
@@ -27,7 +27,7 @@ print("Total number of inliers = "+str(countInliers))
 
 FinalData2 = FinalData.copy()
 #remove outliers
-for i in range(30000):
+for i in range(300):
     if outlierArray[i] == -1:
         FinalData.drop(i, inplace = True)
 
